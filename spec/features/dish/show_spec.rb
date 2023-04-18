7b507d7730a73db8e68e896be6a3ff65627da60c
Logan Cole <emtlogancole@gmail.com>
2023-04-17 19:55:47 -0500

    test: dish total calories method

--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe "dish show page" do
   before :each do
     @chef_1 = Chef.create!(name: "Logan")
-    @dish_1 = @chef_1.dishes.create!(name: "Bread", description: "It's bread")
     @ingredient_1 = Ingredient.create!(name: "bread", calories: 100)
+    @dish_1 = @chef_1.dishes.create!(name: "Bread", description: "It's bread")
+    DishIngredient.create!(dish: @dish_1, ingredient: @ingredient_1)
   end
   it "show's dishes name, calories description" do
     visit "/dishes/#{@dish_1.id}"
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe Dish, type: :model do
   describe "relationships" do
     it {should belong_to :chef}
   end
+
+  before :each do
+    @chef_1 = Chef.create!(name: "Logan")
+    @ingredient_1 = Ingredient.create!(name: "bread", calories: 100)
+    @dish_1 = @chef_1.dishes.create!(name: "Bread", description: "It's bread")
+    DishIngredient.create!(dish: @dish_1, ingredient: @ingredient_1)
+  end
+
+  it 'totals calories' do
+    expect(@dish_1.total_calories).to eq(100)
+  end
 end
